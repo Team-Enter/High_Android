8569fd8aa886913be350f08aded8c52f65cb92b9
hseun <hseun0732@dsm.hs.kr>
2024-02-21 00:28:57 +0900

    add :: ApiProvider

---
+++ app/src/main/java/com/enter/high_v1/ApiProvider.kt
@@ -0,0 +1,19 @@
+package com.enter.high_v1
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiProvider {
+    private var instance: Retrofit? = null
+    private val BASE_URL = "http://10.0.2.2:8080"
+
+    fun getInstance(): Retrofit {
+        if (instance == null) {
+            instance = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+        return instance!!
+    }
+}
