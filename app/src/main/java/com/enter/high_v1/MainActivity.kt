680e06fcda7cc5bea29066d03d89f983a80f2d41
hseun <hseun0732@dsm.hs.kr>
2024-02-25 04:31:11 +0900

    fix :: MainActivity

--- app/src/main/java/com/enter/high_v1/MainActivity.kt
+++ app/src/main/java/com/enter/high_v1/MainActivity.kt
@@ -8,21 +8,29 @@ import androidx.fragment.app.Fragment
 import com.enter.high_v1.databinding.ActivityMainBinding
 import com.enter.high_v1.home.HomeFragment
 import com.enter.high_v1.inspection.InspectionQuestFragment
+import com.enter.high_v1.inspection.InspectionResultFragment
+import com.enter.high_v1.inspection.InspectionStartFragment
 import com.enter.high_v1.mypage.MyPageData
 import com.enter.high_v1.mypage.MyPageFragment
+import com.enter.high_v1.school.InfoDepartFragment
+import com.enter.high_v1.school.InfoSummaryFragment
+import com.enter.high_v1.school.SchoolInfoFragment
+import com.enter.high_v1.school.SchoolInfoResponse
+import com.enter.high_v1.school.SchoolRecommendFragment
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    lateinit var userData: MyPageData
+    var userData: MyPageData = MyPageData("user", "푕힁영", "email")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        addFragment(0)
+        // addFragment(0)
+        addFragment(3)
     }
 
     fun addFragment(index: Int) {
@@ -30,10 +38,31 @@ class MainActivity : AppCompatActivity() {
         val transaction = fragmentManager.beginTransaction()
         when(index) {
             0 -> transaction.replace(R.id.lay_main_frame, HomeFragment())
-            1 -> transaction.replace(R.id.lay_main_frame, InspectionQuestFragment())
+            1 -> transaction.replace(R.id.lay_main_frame, InspectionStartFragment())
             2 -> transaction.replace(R.id.lay_main_frame, MyPageFragment())
+            3 -> transaction.replace(R.id.lay_main_frame, InspectionQuestFragment())
+
+            5 -> transaction.replace(R.id.lay_main_frame, InspectionResultFragment())
+            6 -> transaction.replace(R.id.lay_main_frame, SchoolRecommendFragment())
         }
         transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    fun schoolInfo(name: String) {
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.lay_main_frame, SchoolInfoFragment(name))
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    fun schoolDetailInfo(index: Int, schoolInfo: SchoolInfoResponse) {
+        val transaction = supportFragmentManager.beginTransaction()
+        when(index) {
+            0 -> transaction.replace(R.id.lay_info_frame, InfoSummaryFragment(schoolInfo))
+            1 -> transaction.replace(R.id.lay_info_frame, InfoDepartFragment(schoolInfo))
+        }
         transaction.commit()
     }
 }
